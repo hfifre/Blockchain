0e036d3cae3d2c3c9f9e324c5a220c6d70057306
hfifre <hugo.fifre@gmail.com>
2023-12-05 10:09:42 +0100

    Block & Blockchain classes

---
+++ Block.py
@@ -0,0 +1,6 @@
+class Block:
+  
+  def __init__(self, data, previous_hash):
+    self.data = data
+    self.previous_hash = previous_hash
+    self.block_hash = "test"
---
+++ Blockchain.py
@@ -0,0 +1,23 @@
+from Block import Block
+
+class Blockchain:
+  
+  def __init__(self):
+    self.chain = []
+    self.generate_first_block()
+
+  def generate_first_block(self):
+    self.chain.append(Block("first_block", "0"))
+  
+  def create_block_in_chain(self, data):
+    previous_block_hash = self.last_block.block_hash
+    self.chain.append(Block("test",previous_block_hash))
+  
+  def display_blockchain(self):
+    for i in range(len(self.chain)):
+      print(f"Data {i + 1}: {self.chain[i].data}")
+      print(f"Hash {i + 1}: {self.chain[i].block_hash}\n------")
+  
+  @property
+  def last_block(self):
+    return self.chain[-1]
