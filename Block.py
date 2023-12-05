e94a23eb8d9f0756a062339fcdfa2710e77b7097
Khaarn <shrike3emecompagnie@gmail.com>
2023-12-05 12:17:34 +0100

    ajout de data qui concantène le temps utc et une direction pour la voiture, et ajout d'une interface de jeu

--- Block.py
+++ Block.py
@@ -2,7 +2,8 @@ import hashlib
 
 class Block:
   
-  def __init__(self, data, previous_hash):
+  def __init__(self, data, previous_hash, datetime):
     self.data = data
     self.previous_hash = previous_hash
     self.block_hash = hashlib.sha256(self.data.encode()).hexdigest()
+    self.datetime = datetime
--- Blockchain.py
+++ Blockchain.py
@@ -1,4 +1,5 @@
 from Block import Block
+import datetime
 
 class Blockchain:
   
@@ -7,11 +8,11 @@ class Blockchain:
     self.generate_first_block()
 
   def generate_first_block(self):
-    self.chain.append(Block("first_block", "0"))
+    self.chain.append(Block("first_block", "0", datetime.datetime.utcnow))
   
   def create_block_in_chain(self, data):
     previous_block_hash = self.last_block.block_hash
-    self.chain.append(Block(data,previous_block_hash))
+    self.chain.append(Block(data,previous_block_hash, datetime.datetime.utcnow))
   
   def display_blockchain(self):
     for i in range(len(self.chain)):
